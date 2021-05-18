5455944239b8c43648eadcbad91012f825acf58a
Kevin Han <hankevin0618@gmail.com>
2021-05-19 08:41:39 +1000

    login & logout and reload

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,26 +1,28 @@
 import React from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { Redirect, HashRouter as Router, Route, Switch } from "react-router-dom";
 import Auth from "../routes/Auth";
 import Home from "../routes/Home";
 import LoginPage from "../routes/LoginPage";
 import Header from "../components/Header";
 
 
-
 const AppRouter = ({ isLoggedIn, userObj }) => {
-
 
     return (
         <>
             <Router>
-
                 <Switch>
                     {isLoggedIn ?
                         <>
-                            <Header loggedInUser={isLoggedIn} />
+
                             <Route exact path="/">
+                                <Header loggedInUser={isLoggedIn} />
                                 <Home userObj={userObj} />
                             </Route>
+                            <Route exact path="/login" >
+                                <Redirect to="/" />
+                            </Route>
+
                         </>
 
                         :
